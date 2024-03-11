0a7468512d06c01e6287fd077740852dadac1bfb
Sergio Chaparro <sergioechaparro@gmail.com>
2024-03-10 20:26:43 -0500

    Avance módulo Inicio de sesión

--- index.php
+++ index.php
@@ -3,9 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./public/css/index.css">
+    <title>Iniciar Sesión</title>
 </head>
 <body>
-    
+    <header class="header">
+        <div class="header__logo-unal">
+            <img src="./public/img/logo_unal_blanco.webp" 
+            alt="logo Universidad Nacional de Colombia">
+        </div>
+        <nav class="header__menu">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Iniciar Sesión</a></li>
+            </ul>
+        </nav>
+        <div class="header__logo-fce">
+            <img src="./public/img/logo_FCE_blanco.webp" alt="">
+        </div>
+    </header>
+
+    <main class="container__login">
+        <section class="login__form">
+            <h1>
+                Iniciar sesión
+            </h1>
+            <form action="">      
+                <div class="login__form-fields">
+                    <label for="userName">Nombre de Usuario</label>
+                    <input type="text" name="userName" id="userName">
+                </div>
+                
+                <div class="login__form-fields">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" id="password">
+                    <a href="">Olvidé mi contraseña</a>
+                </div>
+
+                <div class="login__form-btn">
+                    <button type="submit">
+                        Ingresar
+                    </button>
+                </div>
+            </form>
+        </section>
+    </main>
+
+    <footer>
+        <section>
+            Enlaces de interés
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </section>
+        <section>
+
+        </section>
+    </footer>
 </body>
 </html>
