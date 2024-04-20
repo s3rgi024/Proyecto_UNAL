57dbc8b2fcf2c36129d8801e5276b05506dac80c
Santiago Alza <santiago2004alza@gmail.com>
2024-04-20 17:36:06 -0500

    Se hace ajuste de estilos, se añaden pantallas de documentación y se añade pantalla de docente para cargar la información.

---
+++ src/views/pages/doc_aprobada.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/doc_aprobada.css">
+    <title>Documentación Aprobada</title>
+</head>
+  <body>
+  
+  <?php   
+    include("../components/navbar.php");
+  ?>
+
+    <section class="main_contratacion__text min-main">
+      <h1>
+        Documentación Aprobada
+      </h1>
+      <br>
+      <p>
+      Este módulo le permite revisar la documentación que ha sido aprobada en el proceso de vinculación con la universidad.      
+      <br>
+      <br>
+        Seleccione una opción a continuación:
+      </p>
+    </section>
+
+    <main class="main bd-grid min-main">
+      
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/hombre.png" alt="" />
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></ion-icon></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ocasional</span>
+            <span class="card__preci card__preci--now">Santiago Alza</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/mujer.png" alt="" />
+        </div>
+        <div class="card__name">
+          <p>Revisar Documentos</p>
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ad-Honorem</span>
+            <span class="card__preci card__preci--now">Juana Martinez</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/hombre.png" alt="" />
+        </div>
+        <div class="card__name">
+          <p>Revisar Documentos</p>
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ocasional</span>
+            <span class="card__preci card__preci--now">Sergio Chaparro</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/mujer.png" alt="" />
+        </div>
+        <div class="card__name">
+          <p>Revisar Documentos</p>
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ad-Honorem</span>
+            <span class="card__preci card__preci--now">Juana Martinez</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/hombre.png" alt="" />
+        </div>
+        <div class="card__name">
+          <p>Revisar Documentos</p>
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ocasional</span>
+            <span class="card__preci card__preci--now">Sergio Chaparro</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card__img">
+          <img src="../../../public/img/mujer.png" alt="" />
+        </div>
+        <div class="card__name">
+          <p>Revisar Documentos</p>
+        </div>
+        <div class="card__precis">
+          <a href="" class="card__icon"><i class='bx bx-info-circle'></i></a>
+          <div>
+            <span class="card__preci card__preci--before">Docente Ad-Honorem</span>
+            <span class="card__preci card__preci--now">Juana Martinez</span>
+          </div>
+          <a href="" class="card__icon"><i class='bx bx-search'></i></a>
+        </div>
+      </article>
+
+    </main>
+    <script src="../../../public/js/navbar.js"></script>
+  </body>
+</html>
---
+++ src/views/pages/vis_docente.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/vis_docente.css">
+    <title>Menú Docentes</title>
+</head>
+<body>
+
+    <?php 
+    
+        include("../components/navbar.php");
+    
+    ?>
+
+    <main class="main_contratacion__container min-main">
+
+        <section class="main_contratacion__text">
+            <h1>
+                Cargue de Información
+            </h1>
+            <br>
+            <p>
+            Bienvenido al sitema de contratación de docentes ocasionales de la facultad de ciencias económicas de la Universidad Nacional de Colombia.
+            <br>
+            <br>
+            Seleccione una opción a continuación:
+            </p>
+        </section>
+
+        <section class="main_contratacion__options">
+            <a href="../../../src/views/pages/vis_arch.php" class="contratacion_option doc_pendiente" style="text-decoration: none; color: inherit;">
+            <div>
+                <img src="../../../public/img/doc_pendiente.webp" alt="persona buscando perfiles">
+                <div class="contratacion_actions">
+                    <span>Documentación Pendiente</span>
+                    <i class='bx bx-info-circle'></i>
+                </div>
+            </div>
+            </a>
+
+            <a href="../../../src/views/pages/doc_aprobada.php" class="contratacion_option doc_aprobada" style="text-decoration: none; color: inherit;">
+            <div>
+                <img src="../../../public/img/doc_aprobada.webp" alt="persona buscando perfiles">
+                <div class="contratacion_actions">
+                    <span>Documentación Aprobada</span>
+                    <i class='bx bx-info-circle'></i>
+                </div>
+            </div>
+            </a>
+
+            <a href="../../../src/views/pages/historial.php" class="contratacion_option doc_historial" style="text-decoration: none; color: inherit;">
+            <div>
+                <img src="../../../public/img/doc_historial.webp" alt="persona buscando perfiles">
+                <div class="contratacion_actions">
+                    <span>Historial</span>
+                    <i class='bx bx-info-circle'></i>
+                </div>
+            </div>
+            </a>
+        </section>
+
+    </main>
+    <script src="../../../public/js/navbar.js"></script>
+</body>
+</html>
