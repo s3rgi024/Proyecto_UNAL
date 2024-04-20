868f014a5b74d6087f21b6099f4e93afacd8b9ee
Sergio Chaparro <sergioechaparro@gmail.com>
2024-04-20 17:41:45 -0500

    cambio en el fondo del menú

--- public/js/main_contratacion.js
+++ public/js/main_contratacion.js
@@ -2,7 +2,7 @@ let doc_pendiente = document.querySelector('.doc_pendiente');
 
 let doc_aprobada = document.querySelector('.doc_aprobada');
 
-let historial = document.querySelector('.historial');
+let doc_historial = document.querySelector('.doc_historial');
 
 
 doc_pendiente.addEventListener('mouseover', function() {
@@ -10,6 +10,26 @@ doc_pendiente.addEventListener('mouseover', function() {
   });
   
 doc_pendiente.addEventListener('mouseout', function() {
+    document.body.style.backgroundColor= "#dadada";
+});
+
+
+
+doc_aprobada.addEventListener('mouseover', function() {
+    document.body.style.backgroundColor= "#edd394";
+  });
+  
+doc_aprobada.addEventListener('mouseout', function() {
+    document.body.style.backgroundColor= "#dadada";
+});
+
+
+
+doc_historial.addEventListener('mouseover', function() {
+    document.body.style.backgroundColor= "#f99485";
+  });
+  
+doc_historial.addEventListener('mouseout', function() {
     document.body.style.backgroundColor= "#dadada";
 });
   
