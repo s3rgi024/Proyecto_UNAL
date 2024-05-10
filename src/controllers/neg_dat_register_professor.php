c6022c0ad13639035d369e28cdf5dce8c702bf16
Sergio Chaparro <sergioechaparro@gmail.com>
2024-05-10 14:09:22 -0500

    Modificación en la lógica del registro de docentes

--- src/controllers/neg_dat_register_professor.php
+++ src/controllers/neg_dat_register_professor.php
@@ -1,8 +1,9 @@
 <?php 
+//error_reporting(E_ALL & ~E_DEPRECATED & ~E_WARNING & ~E_NOTICE);
 
 function createDirs($professorName, $tdoc, $doc){
     //crear carpeta de docente
-    $dir_professor = "../../docs/docentes_ocasionales/" . $professorName . "_" . $tdoc . "_" . $doc;
+    $dir_professor = "../../docs/docentes_ocasionales/" . $professorName . "_" . $doc;
     mkdir($dir_professor);
 
     //crear carpetas de hoja de vida y vinculación
@@ -10,116 +11,35 @@ function createDirs($professorName, $tdoc, $doc){
     mkdir($dir_professor . "/Vinculacion");
 }
 
-function saveFilesProfessor($professorName, $tdoc, $doc, $hoja_vida_func_pub, $decl_jur_ley, $ver_inh_del_sex, $notif_corr_elec,
-$com_ins_vinc, $aut_trat_dat_per, $visa_ext, $fotoc_libr_mil, $tarjeta_prof, $matri_prof, $aval_sst,
-$cert_segun_leng, $experien_lab, $antec_disc_procu, $antec_fisc_contral, $antec_judic_pol_nal, $form_afil_seg_social,
-$form_afil_eps, $form_afil_pension, $cert_cuen_bancaria, $cert_afil_ult_eps, $certi_afil_fond_pensiones,
-$cedula_ciu_ext, $decla_pen_sol_pens_tram, $asig_dia_hor, $reso_nombramiento){
-    
+function saveFilesProfessor($professorName, $doc, $files) {
+
     include ("../../config/db_connection.php");
 
-
-    $generalRoute = "../../docs/docentes_ocasionales/" . $professorName . "_" . $tdoc . "_" . $doc;
-
+    $generalRoute = "../../docs/docentes_ocasionales/" . $professorName . "_" . $doc;
     $routeHojaVida = $generalRoute . "/Hoja de Vida";
     $routeVinculacion = $generalRoute . "/Vinculacion";
 
-    // **Guardar archivos en carpeta Hoja de Vida
-
-    move_uploaded_file($hoja_vida_func_pub["tmp_name"], $routeHojaVida . "/" . $hoja_vida_func_pub["name"]);
-    move_uploaded_file($decl_jur_ley["tmp_name"], $routeHojaVida . "/" . $decl_jur_ley["name"]);
-    move_uploaded_file($ver_inh_del_sex["tmp_name"], $routeHojaVida . "/" . $ver_inh_del_sex["name"]);
-    move_uploaded_file($notif_corr_elec["tmp_name"], $routeHojaVida . "/" . $notif_corr_elec["name"]);
-    move_uploaded_file($com_ins_vinc["tmp_name"], $routeHojaVida . "/" . $com_ins_vinc["name"]);
-    move_uploaded_file($aut_trat_dat_per["tmp_name"], $routeHojaVida . "/" . $aut_trat_dat_per["name"]);
-    move_uploaded_file($aval_sst["tmp_name"], $routeHojaVida . "/" . $aval_sst["name"]);
-    move_uploaded_file($experien_lab["tmp_name"], $routeHojaVida . "/" . $experien_lab["name"]);
-    move_uploaded_file($antec_disc_procu["tmp_name"], $routeHojaVida . "/" . $antec_disc_procu["name"]);
-    move_uploaded_file($antec_fisc_contral["tmp_name"], $routeHojaVida . "/" . $antec_fisc_contral["name"]);
-    move_uploaded_file($antec_judic_pol_nal["tmp_name"], $routeHojaVida . "/" . $antec_judic_pol_nal["name"]);
-
-    //opcionales
-    move_uploaded_file($visa_ext["tmp_name"], $routeHojaVida . "/" . $visa_ext["name"]);
-    move_uploaded_file($fotoc_libr_mil["tmp_name"], $routeHojaVida . "/" . $fotoc_libr_mil["name"]);
-    move_uploaded_file($tarjeta_prof["tmp_name"], $routeHojaVida . "/" . $tarjeta_prof["name"]);
-    move_uploaded_file($matri_prof["tmp_name"], $routeHojaVida . "/" . $matri_prof["name"]);
-    move_uploaded_file($cert_segun_leng["tmp_name"], $routeHojaVida . "/" . $cert_segun_leng["name"]);
-
-
-    // **Guardar archivos en carpeta Vinculación
-
-    move_uploaded_file($form_afil_seg_social["tmp_name"], $routeVinculacion . "/" . $form_afil_seg_social["name"]);
-    move_uploaded_file($form_afil_eps["tmp_name"], $routeVinculacion . "/" . $form_afil_eps["name"]);
-    move_uploaded_file($form_afil_pension["tmp_name"], $routeVinculacion . "/" . $form_afil_pension["name"]);
-    move_uploaded_file($cert_cuen_bancaria["tmp_name"], $routeVinculacion . "/" . $cert_cuen_bancaria["name"]);
-    move_uploaded_file($cert_afil_ult_eps["tmp_name"], $routeVinculacion . "/" . $cert_afil_ult_eps["name"]);
-    move_uploaded_file($certi_afil_fond_pensiones["tmp_name"], $routeVinculacion . "/" . $certi_afil_fond_pensiones["name"]);
-    move_uploaded_file($cedula_ciu_ext["tmp_name"], $routeVinculacion . "/" . $cedula_ciu_ext["name"]);
-
-    //opcionales
-    move_uploaded_file($decla_pen_sol_pens_tram["tmp_name"], $routeVinculacion . "/" . $decla_pen_sol_pens_tram["name"]);
-    move_uploaded_file($asig_dia_hor["tmp_name"], $routeVinculacion . "/" . $asig_dia_hor["name"]);
-    move_uploaded_file($reso_nombramiento["tmp_name"], $routeVinculacion . "/" . $reso_nombramiento["name"]);
-
-
-    $db_connection->begin_transaction();
-
-        try {
-    // Consulta SQL para insertar las rutas de los archivos
-                $sql_insert = "INSERT INTO archivo_doc_oca 
-                    (fk_dni, hoja_vida_func_pub, decl_jur_ley, ver_inh_del_sex, notif_corr_elec, com_ins_vinc, aut_trat_dat_per, 
-                    visa_ext, fotoc_libr_mil, tarjeta_prof, matri_prof, aval_sst, cert_segun_leng, experien_lab, 
-                    antec_disc_procu, antec_fisc_contral, antec_judic_pol_nal, form_afil_seg_social, form_afil_eps, 
-                    form_afil_pension, cert_cuen_bancaria, cert_afil_ult_eps, certi_afil_fond_pensiones, cedula_ciu_ext, 
-                    decla_pen_sol_pens_tram, asig_dia_hor, reso_nombramiento) 
-                    VALUES ('$doc', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    // Preparar la consulta
-                $stmt = $db_connection->prepare($sql_insert);
-
-    // Vincular los parámetros
-                $stmt->bind_param("sssssssssssssssssssssssss", 
-                        $routeHojaVida . "/" . $hoja_vida_func_pub["name"], 
-                        $routeVinculacion . "/" . $decl_jur_ley["name"], 
-                        $routeVinculacion . "/" . $ver_inh_del_sex["name"], 
-                        $routeVinculacion . "/" . $notif_corr_elec["name"], 
-                        $routeVinculacion . "/" . $com_ins_vinc["name"], 
-                        $routeVinculacion . "/" . $aut_trat_dat_per["name"], 
-                        $routeVinculacion . "/" . $visa_ext["name"], 
-                        $routeVinculacion . "/" . $fotoc_libr_mil["name"], 
-                        $routeVinculacion . "/" . $tarjeta_prof["name"], 
-                        $routeVinculacion . "/" . $matri_prof["name"], 
-                        $routeVinculacion . "/" . $aval_sst["name"], 
-                        $routeVinculacion . "/" . $cert_segun_leng["name"], 
-                        $routeVinculacion . "/" . $experien_lab["name"], 
-                        $routeVinculacion . "/" . $antec_disc_procu["name"], 
-                        $routeVinculacion . "/" . $antec_fisc_contral["name"], 
-                        $routeVinculacion . "/" . $antec_judic_pol_nal["name"], 
-                        $routeVinculacion . "/" . $form_afil_seg_social["name"], 
-                        $routeVinculacion . "/" . $form_afil_eps["name"], 
-                        $routeVinculacion . "/" . $form_afil_pension["name"], 
-                        $routeVinculacion . "/" . $cert_cuen_bancaria["name"], 
-                        $routeVinculacion . "/" . $cert_afil_ult_eps["name"], 
-                        $routeVinculacion . "/" . $certi_afil_fond_pensiones["name"], 
-                        $routeVinculacion . "/" . $cedula_ciu_ext["name"], 
-                        $routeVinculacion . "/" . $decla_pen_sol_pens_tram["name"], 
-                        $routeVinculacion . "/" . $asig_dia_hor["name"], 
-                        $routeVinculacion . "/" . $reso_nombramiento["name"]);
-
-            // Ejecutar la consulta
-            $stmt->execute();
-
-            // Confirmar la transacción
-            $db_connection->commit();
-            
-            echo "Inserciones exitosas.";
-
-        } catch (Exception $e) {
-            // Revertir la transacción en caso de error
-            $db_connection->rollback();
-            echo "Error: " . $e->getMessage();
+    foreach ($files as $key => $file) {
+        if (!is_array($file) || !isset($file['tmp_name'])) {
+            continue; // Si el archivo no está presente, pasar al siguiente
         }
+        $folderNumber = substr($key, 0, 1);
+        // Determinar la carpeta de destino basándose en el número
+        $destination = ($folderNumber === '1') ? $routeHojaVida : $routeVinculacion;
+        // Otorgar permisos de escritura a la carpeta de destino
+        chmod($destination, 0777); // 0777 otorga permisos completos, puedes ajustarlo según tus necesidades de seguridad
+
+        echo $destination;
+        move_uploaded_file($file["tmp_name"], $destination . "/" . $file["name"]);
+        $$key = $destination . "/" . $file["name"]; // Asignar ruta del archivo a una variable con el nombre correspondiente
+    }
+
+    // Insertar en la base de datos usando las variables creadas arriba
+   
+
+    echo "Inserciones exitosas.";
 }
+
 
 
 function insertBasicDataProfessor($tdoc, $doc, $name1, $name2, $lName1,
@@ -162,12 +82,41 @@ $cedula_ciu_ext, $decla_pen_sol_pens_tram, $asig_dia_hor, $reso_nombramiento){
     //se crean las carpetas del profesor
     createDirs($name1 . $name2, $tdoc, $doc);
 
+
+    $files = $files = array(
+        "1_hoja_vida_func_pub" => $hoja_vida_func_pub,
+        "1_decl_jur_ley" => $decl_jur_ley,
+        "1_ver_inh_del_sex" => $ver_inh_del_sex,
+        "1_notif_corr_elec" => $notif_corr_elec,
+        "1_com_ins_vinc" => $com_ins_vinc,
+        "1_aut_trat_dat_per" => $aut_trat_dat_per,
+        "1_visa_ext" => $visa_ext,
+        "1_fotoc_libr_mil" => $fotoc_libr_mil,
+        "1_tarjeta_prof" => $tarjeta_prof,
+        "1_matri_prof" => $matri_prof,
+        "1_aval_sst" => $aval_sst,
+        "1_cert_segun_leng" => $cert_segun_leng,
+        "1_experien_lab" => $experien_lab,
+        "1_antec_disc_procu" => $antec_disc_procu,
+        "1_antec_fisc_contral" => $antec_fisc_contral,
+        "1_antec_judic_pol_nal" => $antec_judic_pol_nal,
+
+
+        "2_form_afil_seg_social" => $form_afil_seg_social,
+        "2_form_afil_eps" => $form_afil_eps,
+        "2_form_afil_pension" => $form_afil_pension,
+        "2_cert_cuen_bancaria" => $cert_cuen_bancaria,
+        "2_cert_afil_ult_eps" => $cert_afil_ult_eps,
+        "2_certi_afil_fond_pensiones" => $certi_afil_fond_pensiones,
+        "2_cedula_ciu_ext" => $cedula_ciu_ext,
+        "2_decla_pen_sol_pens_tram" => $decla_pen_sol_pens_tram,
+        "2_asig_dia_hor" => $asig_dia_hor,
+        "2_reso_nombramiento" => $reso_nombramiento
+    );
     //se guardan los archivos en el directorio creado
-    saveFilesProfessor($name1 . $name2, $tdoc, $doc, $hoja_vida_func_pub, $decl_jur_ley, $ver_inh_del_sex, $notif_corr_elec,
-    $com_ins_vinc, $aut_trat_dat_per, $visa_ext, $fotoc_libr_mil, $tarjeta_prof, $matri_prof, $aval_sst,
-    $cert_segun_leng, $experien_lab, $antec_disc_procu, $antec_fisc_contral, $antec_judic_pol_nal, $form_afil_seg_social,
-    $form_afil_eps, $form_afil_pension, $cert_cuen_bancaria, $cert_afil_ult_eps, $certi_afil_fond_pensiones,
-    $cedula_ciu_ext, $decla_pen_sol_pens_tram, $asig_dia_hor, $reso_nombramiento);  
+    saveFilesProfessor($name1 . $lName1, $doc, $files);
+
+
 }
 
 execute_register_professor($_POST["t_doc_register"], $_POST["dni_register_professor"], $_POST["name1_professor"], 
