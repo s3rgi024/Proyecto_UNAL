a86f05694684e925917039ab48c4165694e7cc07
Sergio <sergioechaparro@gmail.com>
2025-05-27 16:31:19 -0500

    se modificó la migración de users para aceptar un mayor número de documentos

--- database/migrations/2025_03_11_000009_create_users_table.php
+++ database/migrations/2025_03_11_000009_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('fk_type_dni')->index();
-            $table->unsignedBigInteger('dni')->index()->unique();
+            $table->string('dni')->index()->unique();
             $table->string('first_name', 50);
             $table->string('second_name', 50)->nullable();
             $table->string('first_surname', 50);
